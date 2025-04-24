58dd24ef28930b0999086b5f257319f962a5769d
ArriagaArturo <ara872@g.harvard.edu>
2025-04-24 18:43:52 -0500

    Initial Commit

---
+++ SpeechTherapy/SpeechTherapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechTherapyApp.swift
+//  SpeechTherapy
+//
+//  Created by Art Arriaga on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechTherapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeechTherapyTests/SpeechTherapyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpeechTherapyTests.swift
+//  SpeechTherapyTests
+//
+//  Created by Art Arriaga on 4/24/25.
+//
+
+import Testing
+
+struct SpeechTherapyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
